14724b15d46d7d5e649e245fddd660f4cd5ddad4
Doug Bennett <dben89x@gmail.com>
2014-11-18 13:51:35 -0700

    stuff

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -1,3 +1,12 @@
 class Employment < ActiveRecord::Base
+  belongs_to :organization
+  belongs_to :job_title
+  belongs_to :location
+  belongs_to :person
+
+  validates :job_title_id, presence: true
+  validates :location_id, presence: true
+  validates :person_id, presence: true
+  validates :organization_id, presence: true
 
 end
